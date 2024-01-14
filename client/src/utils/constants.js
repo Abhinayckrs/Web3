62d242d4d2c1de1a424b1d286d791e5c8e7702db
Abhinay Kumar <123803356+Abhinayckrs@users.noreply.github.com>
2024-01-14 16:00:59 +0530

    constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,4 +2,4 @@ import abi from './Transactions.json';
 
 
 export const contractABI =abi.abi;
-export const contractAddress = '0x0E8171cDCe667fe148C2854cF37e57BD6fA3f92E';
+export const contractAddress = '';  //fill your own address
